ebfaf30644d096dbbfc2578aeb4834eb19b25a29
Richard Yang <richard.ruoyu.yang@gmail.com>
2023-09-13 17:31:41 -0700

    changed key for future stores

--- src/future_store.h
+++ src/future_store.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#include <boost/uuid/uuid.hpp>
+#include <unordered_map>
+#include <future>
+#include <msgpack.hpp>
+
+#include "object_ref.h"
+
 namespace rayclone {
 class FutureStore {
 public:
@@ -10,6 +17,11 @@ public:
 
   template <typename ReturnType> void AddFuture(ObjectRef<ReturnType> ob_ref) {}
 
+  std::future<msgpack::sbuffer> GetFuture(boost::uuids::uuid id){
+    return std::move(future_map.at(id));
+  }
+
 private:
+  std::unordered_map<boost::uuids::uuid, std::future<msgpack::sbuffer>, boost::hash<boost::uuids::uuid>> future_map;
 };
 } // namespace rayclone
