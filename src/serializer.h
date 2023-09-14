f6815b642b9d6ba6a76b3c5ed566a9e0055ad656
Richard Yang <richard.ruoyu.yang@gmail.com>
2023-09-14 15:51:56 -0700

    use move semantics for msgpack::sbuffer

--- src/serializer.h
+++ src/serializer.h
@@ -8,13 +8,13 @@ public:
   template <typename T> static msgpack::sbuffer Serialize(const T &arg) {
     msgpack::sbuffer buf;
     msgpack::pack(buf, arg);
-    return buf;
+    return std::move(buf);
   }
 
   template <typename T> static T Deserialize(const msgpack::sbuffer &buf) {
     T res;
     msgpack::unpack(buf.data(), buf.size()).get().convert(res);
-    return res;
+    return std::move(res);
   }
 };
 } // namespace rayclone
