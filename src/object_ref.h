f1a6873c71719dbcfa82c5acb0c9c776397fbab5
Richard Yang <richard.ruoyu.yang@gmail.com>
2023-09-13 17:31:12 -0700

    add copy and assignment constructor to objectref

--- src/object_ref.h
+++ src/object_ref.h
@@ -1,14 +1,20 @@
 #pragma once
 
-#include <boost::uuid/uuid.hpp>
-#include <boost::uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
 
 namespace rayclone {
 template <typename ReturnType> class ObjectRef {
 public:
-  ObjectRef() : tag(boost::uuids::random_generator()) {}
+  ObjectRef() : tag(boost::uuids::random_generator()()) {}
 
   bool operator==(ObjectRef const &rhs) const { return tag == rhs.tag; }
+
+  ObjectRef(ObjectRef const& rhs): tag(rhs.tag){}
+
+  ObjectRef& operator=(ObjectRef const& rhs){
+    tag = rhs.tag;
+  }
 
 private:
   boost::uuids::uuid tag;
