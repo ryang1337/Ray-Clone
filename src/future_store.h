358bcb28fc33547130c071e3aeee5b1511c7ec5c
Richard Yang <richard.ruoyu.yang@gmail.com>
2023-09-15 13:45:20 -0700

    improved future_store

--- src/future_store.h
+++ src/future_store.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #include <boost/uuid/uuid.hpp>
-#include <unordered_map>
 #include <future>
 #include <msgpack.hpp>
+#include <unordered_map>
+#include <utility>
 
 #include "object_ref.h"
 
@@ -15,13 +16,23 @@ public:
     return fs;
   }
 
-  template <typename ReturnType> void AddFuture(ObjectRef<ReturnType> ob_ref) {}
+  template <typename ReturnType>
+  bool AddFuture(const ObjectRef<ReturnType> &ob_ref,
+                 std::future<msgpack::sbuffer> &&fut) {
+    return future_map
+        .emplace(ob_ref.GetTag(),
+                 std::forward<std::future<msgpack::sbuffer>>(fut))
+        .second;
+  }
 
-  std::future<msgpack::sbuffer> GetFuture(boost::uuids::uuid id){
-    return std::move(future_map.at(id));
+  template <typename ReturnType>
+  std::future<msgpack::sbuffer> GetFuture(const ObjectRef<ReturnType> &ob_ref) {
+    return std::move(future_map.at(ob_ref.GetTag()));
   }
 
 private:
-  std::unordered_map<boost::uuids::uuid, std::future<msgpack::sbuffer>, boost::hash<boost::uuids::uuid>> future_map;
+  std::unordered_map<boost::uuids::uuid, std::future<msgpack::sbuffer>,
+                     boost::hash<boost::uuids::uuid>>
+      future_map;
 };
 } // namespace rayclone
