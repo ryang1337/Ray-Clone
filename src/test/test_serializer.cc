fa3ba6203f224b773194bba94b8a0c643693bbe2
Richard Yang <richard.ruoyu.yang@gmail.com>
2023-09-14 17:58:58 -0700

    added test for serializer

---
+++ src/test/test_serializer.cc
@@ -0,0 +1,33 @@
+#include <gtest/gtest.h>
+#include <src/serializer.h>
+#include <msgpack.hpp>
+
+TEST(SerializerTest, SerializeIntTest){
+  msgpack::sbuffer buf = rayclone::Serializer::Serialize(5);
+  int val = 5;
+  msgpack::sbuffer expected;
+  msgpack::pack(expected, val);
+  EXPECT_EQ(*buf.data(), *expected.data());
+}
+
+TEST(SerializerTest, SerializeStringTest){
+  msgpack::sbuffer buf = rayclone::Serializer::Serialize("test");
+  std::string val = "test";
+  msgpack::sbuffer expected;
+  msgpack::pack(expected, val);
+  EXPECT_EQ(*buf.data(), *expected.data());
+}
+
+TEST(SerializerTest, DeserializeIntTest){
+  msgpack::sbuffer packed;
+  msgpack::pack(packed, 5);
+  int unpacked = rayclone::Serializer::Deserialize<int>(packed);
+  EXPECT_EQ(unpacked, 5);
+}
+
+TEST(SerializerTest, DeserializeStringTest){
+  msgpack::sbuffer packed;
+  msgpack::pack(packed, "test");
+  std::string unpacked = rayclone::Serializer::Deserialize<std::string>(packed);
+  EXPECT_EQ(unpacked, "test");
+}
