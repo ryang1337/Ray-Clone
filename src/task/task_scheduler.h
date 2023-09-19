269fd3664a4625698358d306c4d979a2ce211e8f
Richard Yang <richard.ruoyu.yang@gmail.com>
2023-09-18 17:22:54 -0700

    add submitter field to scheduler

--- src/task/task_scheduler.h
+++ src/task/task_scheduler.h
@@ -6,15 +6,20 @@
 #include <src/common_types.h>
 
 #include "task_spec.h"
+#include "task_submitter.h"
 
 namespace rayclone {
 
 class TaskScheduler {
 public:
-  TaskScheduler(){};
+  TaskScheduler(std::unique_ptr<TaskSubmitter> client)
+      : client_(std::move(client)){};
 
-  virtual ~TaskScheduler(){};
+  virtual ~TaskScheduler() = default;
 
   virtual std::future<msgpack::sbuffer> ScheduleTask(TaskSpec task_spec) = 0;
+
+protected:
+  std::unique_ptr<TaskSubmitter> client_;
 };
 } // namespace rayclone
